20043305c260cfe59fed2cf97f3d681e8dbe1b47
Franco Javier Williams <fjw@Francos-MacBook-Pro.local>
2020-02-18 15:10:28 -0500

    parameters added

---
+++ default-params/settings.py
@@ -0,0 +1,9 @@
+#global parameters
+
+parameters_correlations = [LIST_STOCKS, LIST_WEEKS, LIST_WINDOWS, DATE_END, DATE_DEFAULT]
+
+info_database = [URL, PROPERTIES, MODE, TABLE]
+
+path_start = PATH_TO_S3
+
+final_date = FINAL_DATE
