3aabd4839f67b95fa1b75b26a67da964a7e0f052
Your Name <you@example.com>
2020-02-24 18:33:36 -0500

    added docstrings

--- stats-functions/stats_functions.py
+++ stats-functions/stats_functions.py
@@ -1,49 +1,32 @@
-import correlation_tables as ct
-import write_to_database as wd
-import settings as st
+import math
 
 
-def dict_of_stocks(list_stocks):
+def stdf(vec):
     """
-    Returns a dictionary of DataFrames
+    Custom standard deviation of a vector [v1,...,vn]: namely sqrt(v1^2+...+vn^2).
+    """
     
-    :type: list[str]                :
-    :rtype: dict[DataFrame]         :
-    .
+    prod = [x*x for x in vec]
+    s = sum(prod)/len(vec)
+    a = (sum(vec)/len(vec))**2
+    return math.sqrt(s-a)
+
+
+def correl(vec1,vec2):
     """
-    dict_dfs = {}
-    for stock in list_stocks:
-    dict_dfs[stock]=ct.get_data_pers(stock)
-    return dict_dfs
+    Custom correlation of two vectors vec1, vec2.
+    Returns 0 when they don't have the same lenght
+    """
     
-
-def main_method(list_stocks, list_weeks, list_windows, date_end, date_default, url, properties, mode, table):
-    """
-    Writes a table to postgres from the correlation_tables method
-    
-    :type list_weeks  : list[int]         List of weeks               
-    :type list_windows: list[int]         List of window sizes in days 
-    :type date_end    : datetime          to end_date
-    :type date_default: datetime          from a fixed default date
-    :type url         : str               postgres url in write_to_database
-    :type properties  : str               postgres properties in write_to_database
-    :type mode        : str               equals "overwrite"
-    :type table       : str               name of table
-    :rtype            : list[list]        postgres table
-    .
-    """
-    dict_dfs = dict_of_stocks(list_stocks)
-    table_return = ct.table_of_all_corrs(dict_dfs, list_stocks, list_weeks, list_windows, date_end, date_default)
-    wd.write_to_database(table_return, url, properties, mode, table)
-
-
-if "__name__" == "__main__":
-    """Using the global parameters in settings."""
-    params = st.parameters_correlation
-    info = st.info_database
-    main_method(params[0],params[1],params[2],params[3],params[4],info[0], info[1], info[2], info[3])
-
-
+    l1=len(vec1)
+    if len(vec1)==len(vec2):
+        prod = list(map(lambda x,y: x*y,vec1,vec2))
+        a1 = sum(vec1)/l1
+        a2 = sum(vec2)/l1
+        a3 = sum(prod)/l1
+        return (a3-a1*a2)/(stdf(vec1)*stdf(vec2))
+    else:
+        return 0
 
     
     
