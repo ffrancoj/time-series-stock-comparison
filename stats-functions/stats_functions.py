95b986b73fda1e524a76c96da0e1c18e85b100ee
Your Name <you@example.com>
2020-02-24 18:36:24 -0500

    added docstrings

--- stats-functions/stats_functions.py
+++ stats-functions/stats_functions.py
@@ -2,9 +2,8 @@ import math
 
 
 def stdf(vec):
-    """
-    Custom standard deviation of a vector [v1,...,vn]: namely sqrt(v1^2+...+vn^2).
-    """
+    
+    """ Custom standard deviation of a vector [v1,...,vn]: namely sqrt(v1^2+...+vn^2)."""
     
     prod = [x*x for x in vec]
     s = sum(prod)/len(vec)
@@ -13,10 +12,8 @@ def stdf(vec):
 
 
 def correl(vec1,vec2):
-    """
-    Custom correlation of two vectors vec1, vec2.
-    Returns 0 when they don't have the same lenght
-    """
+    
+    """ Custom correlation of two vectors vec1, vec2. Returns 0 when they don't have the same length."""
     
     l1=len(vec1)
     if len(vec1)==len(vec2):
